194da4d1b6f9c141973c465e84671168a290a988
chinhphan099 <chinhphan099@gmail.com>
2019-04-02 12:39:27 +0700

    variable

---
+++ js-variable.js
@@ -0,0 +1,12 @@
+// Bài tập
+// Khai báo 2 biến x, y nhận giá trị số bất kì
+// Khai báo các biến khác và lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.
+
+var x = 10,
+  y = 5,
+  tong = x + y,
+  hieu = x - y,
+  tich = x * y,
+  thuong = x / y;
+  
+console.log(tong, hieu, tich, thuong);
