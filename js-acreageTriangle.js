874b6cb732445fa55595bad4ccfaadbe58073a95
chinhphan099 <chinhphan099@gmail.com>
2019-04-02 13:18:16 +0700

    create js-acreageTriangle.js

---
+++ js-acreageTriangle.js
@@ -0,0 +1,10 @@
+/**
+ * a: cạnh đáy của 1 hình tam giác
+ * h: chiều cao của tam giác đó
+ * Yêu cầu: khai báo biến s bằng diện tích của hình tam giác này 
+ */
+ 
+var a = 10;
+var h = 20;
+var s = (a * h) / 2;
+console.log(s);
