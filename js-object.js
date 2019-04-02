ba0f1d3f78cec05f8274370388a1550416cdf7ef
chinhphan099 <chinhphan099@gmail.com>
2019-04-02 12:55:16 +0700

    Create js-object.js

---
+++ js-object.js
@@ -0,0 +1,19 @@
+/**
+ * Sử dụng kiến thức đã học khai báo một vài object khác nhau mà bạn có thể tưởng tượng ra. Ví dụ:
+ *  var myCommunity = {
+ *    name: 'Coders.Tokyo',
+ *    numberOfMembers: 10000,
+ *    isAwesome: true
+ *  };
+ */
+
+var info = {
+  name: 'Chinh',
+  hairColor: 'black'
+};
+
+var character = {
+  isTalkative: false,
+  isHandsome: true,
+  isRich: false
+};
