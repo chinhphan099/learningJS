cc4824e7c00b9c89397226142682fee439fdc615
chinhphan099 <chinhphan099@gmail.com>
2019-04-02 13:12:20 +0700

    Create js-array.js

---
+++ js-array.js
@@ -0,0 +1,13 @@
+/**
+ * Cách sử dụng Array và Object khác nhau như thế nào. Viết câu trả lời ở phía dưới.
+ */
+/*
+  Lấy giá trị trong array dựa vào index. Thường được dùng để khai báo 1 list các giá trị giống nhau.
+  Lấy giá trị trong object dựa vào tên. Thường được dùng để khai báo 1 đối tượng gồm nhiều thuộc tính / kiểu dữ liệu khác nhau.
+*/
+
+// Bài tập từ Video
+var staffs = [
+  {name: "Nhu", position: "Tester", age: "18"},
+  {name: "Phuong", position: "Dev", age: "30"}
+];
