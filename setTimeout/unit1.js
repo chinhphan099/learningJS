b2c2957da70b233367c320d6815d8606cece04e6
Chinh Phan <chinhphan099@gmail.com>
2019-04-15 23:03:59 +0700

    settimeout and promise

---
+++ setTimeout/unit1.js
@@ -0,0 +1,14 @@
+/**
+ * Tạo 1 hàm doAfter nhận vào 2 tham số:
+ *  - Tham số thứ 1: 1 function
+ *  - Tham số thứ 2: Thời gian x (ms)
+ * Hàm này sẽ gọi function sau 1 khoảng thời gian x ms
+ */
+
+var done = function() {
+  console.log('done');
+};
+var doAfter = function(callback, time) {
+  setTimeout(callback, time);
+};
+doAfter(done, 1000);
---
+++ setTimeout/unit2.js
@@ -0,0 +1,29 @@
+/**
+ * Tạo 1 hàm doAfter nhận vào 2 tham số:
+ *  - Tham số thứ 1: 1 function
+ *  - Tham số thứ 2: Thời gian x (ms)
+ * Hàm này sẽ gọi function sau 1 khoảng thời gian x ms VÀ trả về 1 promise để có thể gọi như sau
+ */
+
+function doAfter(callback, time) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      callback();
+      resolve();
+    }, time);
+  });
+}
+
+function sayHello() {
+  console.log('Hello');
+}
+
+function sayGoodbye() {
+  console.log('Goodbye');
+}
+
+doAfter(sayHello, 1000).then(sayGoodbye);
+// Expect:
+// Wait 1s
+// Hello
+// Goodbye
