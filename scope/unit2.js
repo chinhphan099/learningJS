a8e9ca640ecb2386263c93fcfbfc0acc8a8aefdb
Chinh Phan <chinh.phan@dfo.global>
2019-04-17 15:12:38 +0700

    scope

--- scope/unit2.js
+++ scope/unit2.js
@@ -12,3 +12,9 @@ b.foo();
 var fooCopy = b.foo;
 fooCopy();
 // Chạy code và giải thích vì sao kết quả dòng 10 khác dòng 13
+
+/*
+    line 10 - gọi tới hàm foo, this là b => nên this.a = 2;
+    line 12: gán fooCopy = b.foo; this lúc nào là window => this.a = 1;
+    để line 13 cũng ra 2 thì có thể gọi fooCopy.apply(b); hoặc fooCopy.call(b);
+*/
