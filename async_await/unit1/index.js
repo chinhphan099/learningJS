d1aff3b2ee8a75bc4cb4c1b440dbd4bcfe02a0f1
Chinh Phan <chinhphan099@gmail.com>
2019-08-06 07:22:51 +0700

    fetch

--- async_await/unit1/index.js
+++ async_await/unit1/index.js
@@ -42,6 +42,9 @@ var loadDataUrl = function(url) {
   //   .then(function(response) {
   //     return response.json();
   //   })
+  //   .then(function(data) {
+  //      console.log(data);
+  //  })
   //   .catch(function(err) {
   //     console.log(err);
   //   });
